6fc9133304ace512a15752f960a34832aea15889
amitabh Singh <amitabhsingh128@gmail.com>
2026-02-05 12:47:13 +0530

    Methods-L2, problem2:Sum of N natural number.

---
+++ java_training/methods/level_2/problem_3/problem_3.java
@@ -0,0 +1,29 @@
+package java_training.methods.level_2.problem_3;
+
+import java.util.Scanner;
+
+public class problem_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        scanner.close();
+        
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a Leap Year");
+        }
+        
+    }
+    
+    public static boolean isLeapYear(int year) {
+        if (year < 1582) {
+            System.out.println("Warning: The Gregorian calendar was adopted in 1582.");
+            return false;
+        }
+        
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
