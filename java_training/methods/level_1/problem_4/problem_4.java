41ff9fd67b14eebcc6a7d24f6077c78898f826be
amitabh Singh <amitabhsingh128@gmail.com>
2026-02-05 12:12:29 +0530

    Methods, problem4:Positive, Negative, Zero.

---
+++ java_training/methods/level_1/problem_4/problem_4.java
@@ -0,0 +1,19 @@
+package java_training.methods.level_1.problem_4;
+import java.util.*;;
+
+
+public class problem_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(PositiveNegativeZero(sc.nextInt()));
+    }
+
+    public static int PositiveNegativeZero(int num){
+        if(num==0){
+            return 0;
+        }else if(num>0){
+            return 1;
+        }
+        return -1;
+    }
+}
